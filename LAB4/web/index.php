b4e0409450c316345a54033db5b4eb47c42b6acc
KittinanDev <paykung123456789@gmail.com>
2025-12-09 11:18:17 +0700

    Add LAB3 and LAB4 project files

---
+++ LAB4/web/index.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <div id="hello">Hello PHP</div>
+    </body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    
+    <script>
+        document.getElementById("hello").innerHTML="Welcome";
+        
+        $(function(){});
+
+        $(document).ready(function(){
+            $("#hello").html("I LOVE IT");
+        });
+
+    </script>
+</html>
