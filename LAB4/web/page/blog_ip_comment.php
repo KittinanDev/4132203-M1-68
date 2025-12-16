b96ada8aade92ac0248823bb10ea28786e06fadf
KittinanDev <paykung123456789@gmail.com>
2025-12-16 11:13:15 +0700

    Update blog table schema: rename content to comment, update database records

--- LAB4/web/page/blog_ip_comment.php
+++ LAB4/web/page/blog_ip_comment.php
@@ -1,6 +1,25 @@
 <div>
     <form id="fm_blog">
         <input type="text" name="blog">
-        <button type="submit">Save</button>
+        <button type="submit">SAVE</button>
     </form>
 </div>
+
+<script>
+    $("#fm_blog").submit(function(e){
+        e.preventDefault();
+
+
+        $.ajax({
+            url: "/php/blog.php",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function(res){
+                let 
+                alert(res);
+
+            }
+        });
+    });
+</script>
+
--- LAB4/web/php/blog.php
+++ LAB4/web/php/blog.php
@@ -5,16 +5,36 @@ header('Content-type:application/json');
 include("condb.php");
 
 $method = $_SERVER['REQUEST_METHOD'];
-$response = ['status'=>'error','message'=>'Invalida request'];
+$response = ['status'=>'error','message'=>'Invalide request'];
 
 switch($method){
-    case'GET';
-    // get all data
+    case 'GET':
+        //get all data
         $sql = "SELECT * FROM blog ORDER BY id DESC";
         $stmt = $condb->prepare($sql);
         $stmt->execute();
-
+        $result = $stmt->get_result();
+        $blog = [];
         
+        while($row = $result->fetch_assoc()){
+            $blog[] = $row;
+        }
+
+        $response = ['status'=>'success','data'=>$blog];
+
+        break;
+
+    case 'POST':
+        $comment = $_POST['blog'] ?? null;
+        if($comment){
+            $sql = "INSERT INTO blog (comment) VALUES (?)";
+            $stmt = $condb->prepare($sql);
+            $stmt->bind_param("s",$comment);
+            
+            }else{
+                $response = ['status'=>'error','message'=>'Comment is null'];
+            }
+            break;
 }
 
-echo json_encode($method);
+echo json_encode($response);
